9cbaec6b74bd407bbf9ddb13517d42606fa2d22a
Henrique <rick1n.htz@gmail.com>
2025-10-06 11:05:03 -0300

    descentralizando

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,4 @@
 function menushow() {
     let menu_mobile = document.querySelector('.small-menu')
-    if (menu_mobile.classList.contains('open')) {
-        menu_mobile.classList.remove('open')
-    }
-    else {
-        menu_mobile.classList.add('open')
-    }
+    menu_mobile.classList.toggle('open')
 }
